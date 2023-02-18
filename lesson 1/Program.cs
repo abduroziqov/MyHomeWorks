e73ea9ba0f1d586b705fbaed4a0a594c7464ec4d
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-02-18 16:53:09 +0300

    davom etish

---
+++ lesson 1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bu mening C# dagi boshlang'ich darslarim hisoblanadi.");
+            Console.WriteLine("Davom etish uchun xohlagan tugmani bosing... ");
+            Console.ReadLine();
+
+        }
+    }
+}
